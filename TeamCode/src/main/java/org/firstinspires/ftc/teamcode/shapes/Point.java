44c0d52926de3855846595b888080261ffce7cf3
Androo <andrew.coon84@gmail>
2023-07-15 11:48:37 -0600

    added road runner stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shapes/Point.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shapes/Point.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.shapes;
 
 public class Point {
-    long x, y;
-    public Point(long _x, long _y) {
+    int x, y;
+    public Point(int _x, int _y) {
         this.x = _x;
         this.y = _y;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shapes/Rect.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shapes/Rect.java
@@ -4,7 +4,6 @@ public class Rect extends Shape {
     public Point[] points;
     public Rect(Point tl, Point tr, Point bl, Point br) {
         super(tl);
-        Point[] tmppoitns = {tl, tr, bl, br};
-        points = tmppoitns;
+        points = new Point[]{tl, tr, bl, br};
     }
 }
