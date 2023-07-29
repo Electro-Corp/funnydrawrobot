f957bba6e674a99545286e062aaab640049a6e15
Electro-Corp <chinmaytanmay@outlook.com>
2023-07-29 11:29:49 -0600

    finish (no color tho lamo)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Paint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Paint.java
@@ -4,45 +4,39 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
 /*
- * This is a simple routine to test translational drive capabilities.
+ * draw (yay)
  */
 @Config
 @Autonomous(group = "drive")
 public class Paint extends LinearOpMode {
 
-
+    xmltodraw XMLSystem = new xmltodraw("ftc.svg");
     @Override
     public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        while(!isStarted()) { }
 
-
-        //continually update the signal while waiting for the start button
-        while(!isStarted()) {
-
+        List<Trajectory> trajectories = new ArrayList<>();
+        for(int i = 0; i < XMLSystem.points.size(); i++){
+            trajectories.add(drive.trajectoryBuilder(new Pose2d())
+                    .lineTo(new Vector2d(XMLSystem.points.get(i).x, XMLSystem.points.get(i).y))
+                    .build());
         }
-
-
-    }
-
-
-
-    @Override
-    public void onOpened() {
-
-    }
-
-    @Override
-    public void onError(int errorCode) {
-
+        for (Trajectory trajectory:trajectories) {
+            drive.followTrajectory(trajectory);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shapes/Point.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shapes/Point.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode.shapes;
 
 public class Point {
-    int x, y;
+    public int x;
+    public int y;
     public Point(int _x, int _y) {
         this.x = _x;
         this.y = _y;
